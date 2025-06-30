edfb127ef9477ce84ccd996d2458e55578ff9818
Willyan Gomes <119498507+Willgomes86@users.noreply.github.com>
2025-06-30 15:39:08 -0300

    Fix core template integration

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    # Use the full dotted path so Django can correctly locate the app
+    name = 'apps.core'
