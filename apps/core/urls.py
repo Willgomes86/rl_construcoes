fb9b643b7e1e544aaeb81f79123a7c30755265db
Willyan Gomes <willyangomes86@gmail.com>
2025-07-01 07:27:24 -0300

    atualizando

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.listar_clientes, name='listar_clientes'),
-    path('novo/', views.cadastrar_cliente, name='cadastrar_cliente'),
-    path('<int:cliente_id>/', views.visualizar_cliente, name='visualizar_cliente'),
+    path('', views.home, name='home'),
 ]
